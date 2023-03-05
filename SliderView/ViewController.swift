11317584f0e5147a328a44760261eba25959f374
ArtemyDerik <derry0808@gmail.com>
2023-03-05 16:53:27 +0700

    Initial Commit

---
+++ SliderView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SliderView
+//
+//  Created by Артемий Дериглазов on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet var coloredLabel: UIView!
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
